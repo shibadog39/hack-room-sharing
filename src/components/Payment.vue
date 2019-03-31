b56eff70ed1e49e1873730a8c163de2091f4d70f
shibadog1121 <shibadog1121@gmail.com>
2019-03-31 13:00:27 +0900

    display paymentResult

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -16,96 +16,185 @@
           <tr v-for="(item, index) in itemList" :key="index">
             <td>{{item.date | formatDate}}</td>
             <td>{{item.name}}</td>
-            <td>{{item.user.name}}</td>
+            <td>{{getUserById(item.userId).name}}</td>
             <td>{{item.price}}</td>
-            <td><v-btn flat small color="error" @click.prevent="deleteItem(index)">☓</v-btn></td>
+            <td>
+              <v-btn flat small color="error" @click.prevent="deleteItem(index)">☓</v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <span>合計 {{totalAmount}}円</span>
+    </div>
+    <div>
+      <v-form ref="form" lazy-validation>
+        <v-layout row wrap>
+          <v-flex xs3>
+            <v-select
+              v-model.number="newItem.userId"
+              :items="userList"
+              item-text="name"
+              item-value="id"
+              label="人"
+              :rules="[rules.required]"
+            ></v-select>
+          </v-flex>
+          <v-flex xs3>
+            <v-text-field
+              label="モノ・コト"
+              type="text"
+              placeholder="ティッシュ"
+              v-model="newItem.name"
+              :rules="[rules.required]"
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs3>
+            <v-text-field
+              label="値段"
+              type="number"
+              placeholder="216"
+              v-model.number="newItem.price"
+              :rules="[rules.required]"
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs3>
+            <v-btn small color="info" @click.prevent="addItem">add</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-form>
+    </div>
+    <div>
+      <table>
+        <thead>
+          <th></th>
+          <th>負担した金額</th>
+          <th>精算</th>
+        </thead>
+        <tbody>
+          <tr v-for="(result, index) in paymentResult" :key="index">
+            <td>{{getUserById(result.userId).name}}</td>
+            <td>{{result.payBy}}</td>
+            <td>{{result.mustPay | formatMustPay}}</td>
+            <td>
+              <v-btn flat small color="error" @click.prevent="deleteItem(index)">☓</v-btn>
+            </td>
           </tr>
         </tbody>
       </table>
     </div>
-    <v-form  ref="form" lazy-validation>
-        <v-layout row wrap>
-          <v-flex xs4 sm4 md4>
-            <v-select v-model="newItem.user" :items="userList" item-text="name" return-object label="人"></v-select>
-          </v-flex>
-          <v-flex xs4 sm4 md4>
-             <v-text-field label="モノ・コト" type="text" placeholder="ティッシュ" v-model="newItem.name" :rules="[rules.required]"></v-text-field>
-          </v-flex>
-          <v-flex xs4 sm4 md4>
-            <v-text-field label="値段" type="number" placeholder="216" v-model="newItem.price" :rules="[rules.required]"></v-text-field>
-          </v-flex>
-          <v-btn small color="info" @click.prevent="addItem">add</v-btn>
-        </v-layout>
-    </v-form>
+    <pre>{{ $data }}</pre>
   </v-container>
 </template>
 
 <script lang="ts">
-  import { Vue, Component } from 'vue-property-decorator';
+import { Vue, Component } from "vue-property-decorator";
 
-  @Component({
-    filters: {
-      formatDate: (date: Date) => {
-        const m = ('00' + (date.getMonth() + 1)).slice(-2);
-        const d = ('00' + date.getDate()).slice(-2);
-        return m + '/' + d;
-      },
+@Component({
+  filters: {
+    formatDate: (date: Date) => {
+      const m = ("00" + (date.getMonth() + 1)).slice(-2);
+      const d = ("00" + date.getDate()).slice(-2);
+      return m + "/" + d;
     },
-  })
-  export default class Payment extends Vue {
-    private userList: any[] = [
-      {
-        id: 1,
-        name: '櫻井',
-      },
-      {
-        id: 2,
-        name: '尾花',
-      }];
-    private itemList: any[] = [{
-      name: '家賃',
-      user: this.userList[0],
-      price: 100,
-      date: new Date(2019, 0, 1),
-    }];
-    private newItem: any = {
-      name: '',
-      user: this.userList[0],
-      price: 0,
-      date: new Date(),
-    };
-    private  rules: any =  {
-          required: (value: any) => !!value || '入力必須です.'
-    }
-
-    /**
-     * addItem
-     */
-    public addItem() {
-      if(this.validate()){
-        this.newItem.id = this.itemList.length + 1;
-        const newItem = Object.assign({}, this.newItem);
-        this.itemList.push(newItem);
-        (this.$refs.form as any).reset()
+    formatMustPay: (mustPay: number) => {
+      if (mustPay >= 0) {
+        return mustPay + "円支払う";
+      } else {
+        return mustPay * -1 + "円もらう";
       }
     }
-
-    /**
-     * deleteItem
-     */
-    public deleteItem(index: number) {
-      if (confirm('Are you sure?')) {
-        this.itemList.splice(index, 1);
-      }
-    }
-
-    private validate(): boolean {
-      // TODO: remove any
-      return (this.$refs.form as any).validate();
-      }
   }
+})
+export default class Payment extends Vue {
+  private userList: any[] = [
+    {
+      id: 1,
+      name: "櫻井"
+    },
+    {
+      id: 2,
+      name: "尾花"
+    }
+  ];
+  private itemList: any[] = [
+    {
+      name: "家賃",
+      userId: this.userList[0].id,
+      price: 100,
+      date: new Date(2019, 0, 1)
+    }
+  ];
+  private newItem: any = {
+    name: "",
+    userId: this.userList[0].id,
+    price: 0,
+    date: new Date()
+  };
+  private rules: any = {
+    required: (value: any) => !!value || "入力必須です."
+  };
+
+  /**
+   * addItem
+   */
+  private addItem() {
+    if (this.validate()) {
+      const newItem = Object.assign({}, this.newItem);
+      this.itemList.push(newItem);
+      (this.$refs.form as any).reset();
+    }
+  }
+
+  /**
+   * deleteItem
+   */
+  private deleteItem(index: number) {
+    if (confirm("Are you sure?")) {
+      this.itemList.splice(index, 1);
+    }
+  }
+
+  private validate(): boolean {
+    // TODO: remove any
+    return (this.$refs.form as any).validate();
+  }
+
+  private getUserById(userId: number): object {
+    return this.userList.find(user => {
+      return user.id === userId;
+    });
+  }
+  private priceList(itemList: any[]): number[] {
+    return itemList.map(item => {
+      return item.price;
+    });
+  }
+
+  private get totalAmount(): number {
+    return this.priceList(this.itemList).reduce((pre, cur) => {
+      return pre + cur;
+    });
+  }
+
+  private get paymentResult(): any[] {
+    return this.userList.map(user => {
+      const payByItemList = this.itemList.filter(item => {
+        return item.userId === user.id;
+      });
+      const payBy =
+        payByItemList.length === 0
+          ? 0
+          : this.priceList(payByItemList).reduce((pre, cur) => pre + cur);
+      // 切り上げ処理にしてる
+      const mustPay = Math.ceil(
+        this.totalAmount / this.userList.length - payBy
+      );
+
+      return { userId: user.id, payBy: payBy, mustPay: mustPay };
+    });
+  }
+}
 </script>
 
 <style>
-
 </style>
